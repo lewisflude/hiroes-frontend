1a312b8da14ac3c4f58d1e356706c49169b65207
Lewis Flude <lewis.flude@gmail.com>
2016-06-21 16:32:38 +0100

    Add initial ProfileCard

---
+++ components/ProfileCard.js
@@ -0,0 +1,44 @@
+class ProfileCard extends React.Component {
+  getInitialState() {
+    return {
+      name: "",
+      description: "",
+      detail: "",
+      picture: "",
+      talent: "",
+      subtalent: "",
+      minprice: 0,
+      unit: ""
+    };
+  },
+  componentDidMount: function() {
+    this.serverRequest = $.get(this.props.source, function (result) {
+      var userProfile = result[0];
+      this.setState({
+        name: userProfile.Name,
+        description: userProfile.description,
+        detail: userProfile.detail,
+        picture: userProfile.picture,
+        talent: userProfile.talent,
+        subtalent: userProfile.subtalent,
+        minprice: userProfile.minprice,
+        unit: userProfile.unit
+      });
+
+    }.bind(this));
+  },
+  componentWillUnmount: function() {
+    this.serverRequest.abort();
+  },
+  render: function() {
+    return (
+      <div>
+      	<h1>{this.state.name}</h1>
+        <h2>{this.state.talent}</h2>
+        <p>{this.state.description}</p>
+      </div>
+    )
+  }
+}
+
+export default ProfileCard
