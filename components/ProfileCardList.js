34b9648b3983e980c367e1b6ab21ae0cb29a59c4
Lewis Flude <lewis.flude@gmail.com>
2016-06-22 10:38:13 +0100

    Get profile cards from api

---
+++ components/ProfileCardList.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import ProfileCard from './ProfileCard';
+
+class ProfileCardList extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      profiles: []
+    }
+  }
+  componentDidMount() {
+    this.serverRequest = $.getJSON(this.props.source, function (result) {
+      var apiData = $.map(result, function(value, index) { return [value] });
+      this.setState({
+        profiles: apiData
+      });
+
+    }.bind(this));
+  }
+  componentWillUnmount() {
+    this.serverRequest.abort();
+  }
+  render() {
+
+    var profiles = this.state.profiles;
+    return (
+      <div>
+      { profiles.map(function(profile, index){
+        return (
+          <ProfileCard 
+            name={profile.Name}
+            description={profile.description}
+            detail={profile.detail}
+            picture={profile.picture}
+            talent={profile.talent}
+            subtalent={profile.subtalent}
+            minprice={profile.minprice}
+            unit={profile.unit}
+          />
+        )
+      })}
+      </div>
+    )
+  }
+}
+
+export default ProfileCardList;
