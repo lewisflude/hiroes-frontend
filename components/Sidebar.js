1e499edbf3153f1d5fa659d6ae7f08b62c5958f4
Lewis Flude <lewis.flude@gmail.com>
2016-06-23 16:00:37 +0100

    Add initial sidebar nav

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -4,6 +4,32 @@ class Sidebar extends React.Component {
   render() {
     return (
       <div className="sidebar">
+        <div className="nav">
+          <div className="nav-item photography">
+            <a className="nav-item-link">
+              <i className="material-icons md-36">photo_camera</i>
+              <div className="nav-item-label">Photography</div>
+            </a>
+          </div>
+          <div className="nav-item food">
+            <a className="nav-item-link">
+              <i className="material-icons md-36">restaurant</i>
+              <div className="nav-item-label">Food</div>
+            </a>
+          </div>
+          <div className="nav-item music">
+            <a className="nav-item-link">
+              <i className="material-icons md-36">music_note</i>
+            </a>
+            <div className="nav-item-label">Music</div>
+          </div>
+          <div className="nav-item venue">
+            <a className="nav-item-link">
+              <i className="material-icons md-36">home</i>
+            </a>
+            <div className="nav-item-label">Venue</div>
+          </div>
+        </div>
       </div>
     )
   }
