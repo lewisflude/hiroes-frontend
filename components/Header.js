6d6b635e9364b504eaeb6ce43ebfaa97824a2ca1
Lewis Flude <lewis.flude@gmail.com>
2016-06-29 18:36:45 +0100

    Use container in header

--- components/Header.js
+++ components/Header.js
@@ -24,15 +24,17 @@ class Header extends React.Component {
 
     return (
       <header>
-        <div className="header__login-container">
+        <div className="container">
+          <div className="header__login-container">
 
-          <FacebookLogin
-          appId="1553761041593423"
-          autoLoad={true}
-          fields="name,email,picture,gender"
-          callback={responseFacebook} 
-          cssClass="button button--facebook"
-          />
+            <FacebookLogin
+            appId="1553761041593423"
+            autoLoad={true}
+            fields="name,email,picture,gender"
+            callback={responseFacebook} 
+            cssClass="button button--facebook"
+            />
+          </div>
         </div>
       </header>
     )
